d5c25283c7c5259900594ec6c7479fc915267b42
AnhTun-1511 <tuan151104@gmail.com>
2026-01-04 15:29:11 +0700

    thiet ke lai giao dien chi tiet san pham

--- DuAnWeb/pages/main/sanpham.php
+++ DuAnWeb/pages/main/sanpham.php
@@ -1,22 +1,37 @@
-<h2>Chi tiết sản phẩm</h2>
+<h3 class="heading-product">Chi tiết sản phẩm</h3>
 <?php
-    $sql_chitiet= "SELECT * FROM tbl_sanpham, tbl_danhmuc WHERE tbl_sanpham.id_danhmuc = tbl_danhmuc.id_danhmuc 
-    AND tbl_sanpham.id_sanpham = '$_GET[id]' LIMIT 1";
-    $query_chitiet = mysqli_query($mysqli,$sql_chitiet);
+    $sql_chitiet = "SELECT * FROM tbl_sanpham, tbl_danhmuc WHERE tbl_sanpham.id_danhmuc = tbl_danhmuc.id_danhmuc AND tbl_sanpham.id_sanpham = '$_GET[id]' LIMIT 1";
+    $query_chitiet = mysqli_query($mysqli, $sql_chitiet);
     while($row_chitiet = mysqli_fetch_array($query_chitiet)) {
 ?>
 <div class="wrapper_detail">
-    <div class="anh_sanpham">
-        <img width="300px" style="height: auto;" src="admincp/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh']?>">
+    <div class="detail_img">
+        <img src="admincp/modules/quanlysp/uploads/<?php echo $row_chitiet['hinhanh']?>" width="100%">
     </div>
-    <form method="post" action="pages/main/themgiohang.php?idsanpham=<?php echo $row_chitiet['id_sanpham']?>">
-        <div class="chitiet_sanpham">
-            <h3 style="margin: 0;">Tên sản phẩm: <?php echo $row_chitiet['tensanpham']?></h3>
-            <p>Mã sản phẩm: <?php echo $row_chitiet['masanpham']?></p>
-            <p>Giá sản phẩm: <?php echo number_format($row_chitiet['giasanpham'],0,',','.').'VNĐ'?></p>
-            <p>Số lượng sản phẩm: <?php echo $row_chitiet['soluong']?></p>
-            <p>Danh mục sản phẩm: <?php echo $row_chitiet['tendanhmuc']?></p>
-            <p><input class="themgiohang" name="themgiohang" type="submit" value="Thêm vào giỏ hàng"></p>
+
+    <form method="POST" action="pages/main/themgiohang.php?idsanpham=<?php echo $row_chitiet['id_sanpham']?>">
+        <div class="detail_info">
+            <h3 class="detail_name"><?php echo $row_chitiet['tensanpham'] ?></h3>
+            
+            <p class="detail_code">Mã SP: <span><?php echo $row_chitiet['masanpham'] ?></span></p>
+            
+            <p class="detail_price"><?php echo number_format($row_chitiet['giasanpham'],0,',','.').' VNĐ' ?></p>
+            
+            <p class="detail_stock">Số lượng kho: <?php echo $row_chitiet['soluong'] ?></p>
+            
+            <p class="detail_cate">Danh mục: <?php echo $row_chitiet['tendanhmuc'] ?></p>
+            
+            <div class="detail_desc">
+                <b>Mô tả sản phẩm:</b>
+                <p><?php echo $row_chitiet['tomtat'] ?></p>
+                <p><?php echo $row_chitiet['noidung'] ?></p>
+            </div>
+
+            <div class="detail_button">
+                <button type="submit" name="themgiohang" class="btn-add-cart">
+                    <i class="fa fa-shopping-cart"></i> THÊM VÀO GIỎ
+                </button>
+            </div>
         </div>
     </form>
 </div>
